1f4a7320864b5e05f9df1f266d4a70efbdbd4cf2
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-01 15:35:44 +0200

    Initial Commit

---
+++ GuesstheFlag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuesstheFlag
+//
+//  Created by Ihor Sukhachov on 01.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuesstheFlag/GuesstheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesstheFlagApp.swift
+//  GuesstheFlag
+//
+//  Created by Ihor Sukhachov on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuesstheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
