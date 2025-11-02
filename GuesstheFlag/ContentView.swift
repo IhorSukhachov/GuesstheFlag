e97aa1a589fd3a05e4f5ef997041ea0b55eb6a93
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-02 11:54:15 +0200

    angular gradient showvase

--- GuesstheFlag/ContentView.swift
+++ GuesstheFlag/ContentView.swift
@@ -22,8 +22,9 @@ struct ContentView: View {
         }.ignoresSafeArea()*/
         VStack{
         //    LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
-            RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
-        }
+        //    RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
+            AngularGradient(colors: [.blue, .black, .red, .yellow, .purple, .green], center: .center)
+        }.ignoresSafeArea()
         
         
     }
