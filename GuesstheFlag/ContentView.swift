c3a584673e9f56619333b3f5ea5cd8d582a571bb
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-02 12:26:13 +0200

    different button styling showcase

--- GuesstheFlag/ContentView.swift
+++ GuesstheFlag/ContentView.swift
@@ -24,12 +24,26 @@ struct ContentView: View {
         //    LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
         //    RadialGradient(colors: [.blue, .black], center: .center, startRadius: 20, endRadius: 200)
         //    AngularGradient(colors: [.blue, .black, .red, .yellow, .purple, .green], center: .center)
-            Text("Some text").frame(maxWidth: .infinity, maxHeight: .infinity)
-                .foregroundStyle(.white)
+     //       Image(systemName: "pencil.circle.fill").foregroundStyle(.red).font(.largeTitle).padding(200)
+         //   Text("Some text").frame(maxWidth: .infinity, maxHeight: .infinity)
+          //      .foregroundStyle(.white)
+            Button("Edit", systemImage: "pencil") {
+                print("Edit")
+            }.font(.largeTitle).buttonStyle(.bordered)
                 
             Button("Delete All", role:.destructive, action: executeDelete).buttonStyle(.bordered)
+            
+            Button {
+                print("Edit button was tapped")
+            } label: {
+                Label("Edit", systemImage: "pencil")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
+            }
+            
                 
-        }.ignoresSafeArea().background(.black.gradient)
+        }.ignoresSafeArea()
         
         
     }
