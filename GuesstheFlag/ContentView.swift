9e17d5866ccda24b46319d182e8d1c7fdd863ace
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-03 12:33:50 +0200

    added score counter

--- GuesstheFlag/ContentView.swift
+++ GuesstheFlag/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
    @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    
+    @State private var userScore = 0
     var body: some View {
         ZStack {
             //            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
@@ -50,14 +52,14 @@ struct ContentView: View {
                     .clipShape(.rect(cornerRadius:20))
                 Spacer()
                 Spacer()
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
                 Spacer()
             }.alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue", action: askQuestion)
             } message: {
-                Text("Your score is ???")
+                Text("Your score is \(userScore).")
             }.padding()
         }
         
@@ -65,9 +67,10 @@ struct ContentView: View {
     func flagTapped (_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct!"
+            userScore += 1
         }
         else {
-            scoreTitle = "Wrong!"
+            scoreTitle = "Wrong! The correct asnwer is \(countries[correctAnswer])."
         }
         showingScore = true
     }
